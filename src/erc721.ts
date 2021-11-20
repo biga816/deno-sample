b58f8200f11a44812a41071536a70bedf0790b2c
akihiro.tanaka <big.a.0816@gmail.com>
2021-11-20 23:07:04 +0900

    add listening events

--- src/erc721.ts
+++ src/erc721.ts
@@ -3,7 +3,7 @@ import { ethers } from "https://esm.sh/ethers?dts";
 
 import { sendLineNotify } from "./utils/line-helper.ts";
 
-const { INFURA_PROJECT_ID } = config();
+const { INFURA_PROJECT_ID, CONTRACT_ADDRESS } = config();
 
 // const infura = `wss://ropsten.infura.io/ws/v3/${INFURA_PROJECT_ID}`;
 const infura = `wss://mainnet.infura.io/ws/v3/${INFURA_PROJECT_ID}`;
@@ -12,43 +12,42 @@ const provider = new ethers.providers.WebSocketProvider(infura);
 const blockNumber = await provider.getBlockNumber();
 console.log("blockNumber:", blockNumber);
 
-// const address = "0x20fe562d797a42dcb3399062ae9546cd06f63280"; // Link Token
-const address = "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB"; // CryptoPunks
-
 const abi = [
   "event Transfer(address indexed from, address indexed to, uint256 value)",
   "event PunkTransfer(address indexed from, address indexed to, uint256 punkIndex)",
   "event PunkBought(uint indexed punkIndex, uint value, address indexed fromAddress, address indexed toAddress)",
+  "event PunkOffered(uint indexed punkIndex, uint minValue, address indexed toAddress)",
 ];
 
-const erc721 = new ethers.Contract(address, abi, provider);
-
-erc721.on("PunkTransfer", (owner, to, punkIndex, event) => {
-  const { transactionHash } = event;
-  const msg = `${punkIndex} was transferd from ${owner} to ${to}.`;
+const sendNotify = async (
+  msg: string,
+  transactionHash: string,
+  punkIndex: number
+): Promise<void> => {
   console.log(msg);
-  console.log(`etherscan: https://etherscan.io/tx/${transactionHash}`);
+  console.log(`https://etherscan.io/tx/${transactionHash}`);
 
   const punkIndexStr = punkIndex.toString().padStart(4, "0");
   console.log(
-    `url: https://www.larvalabs.com/public/images/cryptopunks/${punkIndexStr}.png`
+    `https://www.larvalabs.com/public/images/cryptopunks/${punkIndexStr}.png`
   );
 
-  sendLineNotify(msg);
+  await sendLineNotify(msg);
+};
+
+const erc721 = new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
+
+erc721.on("PunkTransfer", (owner, to, punkIndex, { transactionHash }) => {
+  const msg = `PunkTransfer: ${punkIndex} was transferd from ${owner} to ${to}.`;
+  sendNotify(msg, transactionHash, punkIndex);
 });
 
-erc721.on("PunkBought", (punkIndex, value, owner, to, event) => {
-  const { transactionHash } = event;
-  const msg = `${punkIndex} was bought from ${owner} to ${to} for ${value} wei.`;
-  console.log(msg);
-  console.log(`etherscan: https://etherscan.io/tx/${transactionHash}`);
-
-  const punkIndexStr = punkIndex.toString().padStart(4, "0");
-  console.log(
-    `url: https://www.larvalabs.com/public/images/cryptopunks/${punkIndexStr}.png`
-  );
-
-  sendLineNotify(msg);
+erc721.on("PunkBought", (punkIndex, value, owner, to, { transactionHash }) => {
+  const msg = `PunkBought: ${punkIndex} was bought from ${owner} to ${to} for ${value} wei.`;
+  sendNotify(msg, transactionHash, punkIndex);
 });
 
-// Deno.exit(0);
+erc721.on("PunkOffered", (punkIndex, minValue, to, { transactionHash }) => {
+  const msg = `PunkOffered: ${punkIndex} was offerd from ${to} for ${minValue} wei.`;
+  sendNotify(msg, transactionHash, punkIndex);
+});
